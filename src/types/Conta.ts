fdc84da2c3e1b29ebfbf6469b8f8ac07a2a8f1f2
Felipe Pacheco <felipe.pacheco@bonin.eng.br>
2025-08-22 09:51:59 -0300

    Criado metedo Armazenamento e atualizado arquivo Conta.js

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -1,11 +1,12 @@
+import { Armazenador } from "./Armazenador.js";
 import { GrupoTransacao } from "./GrupoTransacao.js";
 import { TipoTransacao } from "./TipoTransacao.js";
 import { Transacao } from "./Transacao.js";
 
 export class Conta{
     protected nome: string
-    protected saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0;
-    private transacoes: Transacao[] = JSON.parse(localStorage.getItem("transacoes"), (key: string, value: any) => {
+    protected saldo: number = Armazenador.obter<number>("saldo") || 0;
+    private transacoes: Transacao[] = Armazenador.obter<Transacao[]>(("transacoes"), (key: string, value: any) => {
         if(key === "data"){
             return new Date(value);
         }
@@ -63,7 +64,7 @@ export class Conta{
 
         this.transacoes.push(novaTransacao);
         console.log(this.getGruposTransacoes());
-        localStorage.setItem("transacoes", JSON.stringify(this.transacoes));
+        Armazenador.salvar("transacoes", JSON.stringify(this.transacoes));
     }
 
     debitar(valor: number): void {
@@ -75,7 +76,7 @@ export class Conta{
         }
 
         this.saldo -= valor;
-        localStorage.setItem("saldo", this.saldo.toString());
+        Armazenador.salvar("saldo", this.saldo.toString());
     }
 
     depositar(valor: number): void {
@@ -84,7 +85,7 @@ export class Conta{
         }
 
         this.saldo += valor;
-        localStorage.setItem("saldo", this.saldo.toString());
+        Armazenador.salvar("saldo", this.saldo.toString());
     }
 
 }
