1b70e6d71401b475b3ea608122a2df84ba5af6e5
Felipe Pacheco <felipe.pacheco@bonin.eng.br>
2025-08-22 00:32:12 -0300

    Ajustando código Conta.ts

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -1,49 +1,23 @@
-import { Transacao } from "./Transacao.js";
-import { TipoTransacao } from "./TipoTransacao.js";
-import { GrupoTransacao } from "./GrupoTransacao.js";
+import { GrupoTransacao } from "./GrupoTransacao";
+import { TipoTransacao } from "./TipoTransacao";
+import { Transacao } from "./Transacao";
 
-let saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0;
-const transacoes: Transacao[] = JSON.parse(localStorage.getItem("transacoes"), (key: string, value: string) => {
-    if (key === "data") {
-        return new Date(value);
+export class Conta{
+    nome: string
+    saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0;
+    transacoes: Transacao[] = JSON.parse(localStorage.getItem("transacoes"), (key: string, value: any) => {
+        if(key === "data"){
+            return new Date(value);
+        }
+        return value;
+    }) || [];
+
+    constructor(nome: string) {
+        this.nome = nome;
     }
-
-    return value;
-}) || [];
-
-function debitar(valor: number): void {
-    if (valor <= 0) {
-        throw new Error("O valor a ser debitado deve ser maior que zero!");
-    }
-    if (valor > saldo) {
-        throw new Error("Saldo insuficiente!");
-    }
-
-    saldo -= valor;
-    localStorage.setItem("saldo", saldo.toString());
-}
-
-function depositar(valor: number): void {
-    if (valor <= 0) {
-        throw new Error("O valor a ser depositado deve ser maior que zero!");
-    }
-
-    saldo += valor;
-    localStorage.setItem("saldo", saldo.toString());
-}
-
-const Conta = {
-    getSaldo() {
-        return saldo;
-    },
-
-    getDataAcesso(): Date {
-        return new Date();
-    },
-
     getGruposTransacoes(): GrupoTransacao[] {
         const gruposTransacoes: GrupoTransacao[] = [];
-        const listaTransacoes: Transacao[] = structuredClone(transacoes);
+        const listaTransacoes: Transacao[] = structuredClone(this.transacoes);
         const transacoesOrdenadas: Transacao[] = listaTransacoes.sort((t1, t2) => t2.data.getTime() - t1.data.getTime());
         let labelAtualGrupoTransacao: string = "";
 
@@ -60,24 +34,56 @@ const Conta = {
         }
 
         return gruposTransacoes;
-    },
+    }
+
+    getSaldo(){
+        return this.saldo;
+    }
+
+    getDataAcesso(): Date {
+        return new Date();
+    }
 
     registrarTransacao(novaTransacao: Transacao): void {
         if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
-            depositar(novaTransacao.valor);
+            this.depositar(novaTransacao.valor);
         } 
         else if (novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA || novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO) {
-            debitar(novaTransacao.valor);
+            this.debitar(novaTransacao.valor);
             novaTransacao.valor *= -1;
         } 
         else {
             throw new Error("Tipo de Transação é inválido!");
         }
 
-        transacoes.push(novaTransacao);
+        this.transacoes.push(novaTransacao);
         console.log(this.getGruposTransacoes());
-        localStorage.setItem("transacoes", JSON.stringify(transacoes));
+        localStorage.setItem("transacoes", JSON.stringify(this.transacoes));
     }
+
+    debitar(valor: number): void {
+        if (valor <= 0) {
+            throw new Error("O valor a ser debitado deve ser maior que zero!");
+        }
+        if (valor > this.saldo) {
+            throw new Error("Saldo insuficiente!");
+        }
+
+        this.saldo -= valor;
+        localStorage.setItem("saldo", this.saldo.toString());
+    }
+
+    depositar(valor: number): void {
+        if (valor <= 0) {
+            throw new Error("O valor a ser depositado deve ser maior que zero!");
+        }
+
+        this.saldo += valor;
+        localStorage.setItem("saldo", this.saldo.toString());
+    }
+
 }
 
-export default Conta;
+const conta = new Conta("Joana da Silva Oliveira");
+
+export default conta;
